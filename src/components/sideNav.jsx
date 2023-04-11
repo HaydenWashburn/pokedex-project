33d2e99869fe2037fa3b95637cb44eff6697e974
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-11 08:33:51 -0500

    chagned the margin on the search form

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -3,11 +3,11 @@ function SearchBar(){
 
 return(
     <form className="search-form gameboy">
-        <p>Name</p>
+        <p className="search-header">Name</p>
         <input type="text" name='name' id='name'/>
-        <p>Type</p>
+        <p className="search-header">Type</p>
         <input type="text" name="type" id="type" />
-        <p>Weaknesses</p>
+        <p className="search-header">Weaknesses</p>
         <input type="text" name='weaknesses' id='weaknesses' />
         <button type="submit" className="button gameboy">Search</button>
         </form>
