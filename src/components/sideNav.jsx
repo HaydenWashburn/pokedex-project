80164c8458478aebb6e111e3fed07128e26f0822
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-10 19:54:00 -0500

    added background color to grid container

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,12 +1,14 @@
 function SearchBar(){
+
+
 return(
     <form className="search-form gameboy">
-        <input type="text" name='name' id='name'/>
         <p>Name</p>
-        <input type="text" name="type" id="type" />
+        <input type="text" name='name' id='name'/>
         <p>Type</p>
-        <input type="text" name='weaknesses' id='weaknesses' />
+        <input type="text" name="type" id="type" />
         <p>Weaknesses</p>
+        <input type="text" name='weaknesses' id='weaknesses' />
         <button type="submit" className="button gameboy">Search</button>
         </form>
 )
